c2bd4ca065feedecd35177cc4b4b9bcf229d9353
Neil Charles <neil.d.charles@gmail.com>
2022-03-24 19:46:12 +0000

    fixed imports

--- R/mini_helpers.R
+++ R/mini_helpers.R
@@ -5,16 +5,16 @@ ga_to_points <- function(ga){
 
 ga_points_shapes_lookup <- function(ga_points, shapes = shapes_regions, join_col = 'RGN20CD', population_col = 'mid_2020'){
   traffic_match <- shapes %>%
-    st_join(ga_points) %>%
-    st_drop_geometry()
+    sf::st_join(ga_points) %>%
+    sf::st_drop_geometry()
 
   traffic_regional_summary <- traffic_match %>%
-    group_by_at(join_col) %>%
-    summarise(sessions = sum(sessions)) %>%
-    left_join(populations, by = setNames('code', join_col)) %>%
-    mutate(sessions_per_m = sessions / (!!as.name(population_col)) * 1000000)
+    dplyr::group_by_at(join_col) %>%
+    dplyr::summarise(sessions = sum(sessions)) %>%
+    dplyr::left_join(populations, by = setNames('code', join_col)) %>%
+    dplyr::mutate(sessions_per_m = sessions / (!!as.name(population_col)) * 1000000)
 
   regions %>%
-    left_join(traffic_regional_summary, by = setNames(join_col, join_col))
+    dplyr::left_join(traffic_regional_summary, by = setNames(join_col, join_col))
 
 }
